d612b4985cc84ab60e22efd77685d9806670baf3
nichbomb <nbombard@ualberta.ca>
2019-02-03 14:36:39 -0700

    added the data structures and began work on main activity

--- app/src/main/java/ca/ualberta/nbombard/nbombard_cardiobook/MainActivity.java
+++ app/src/main/java/ca/ualberta/nbombard/nbombard_cardiobook/MainActivity.java
@@ -2,12 +2,66 @@ package ca.ualberta.nbombard.nbombard_cardiobook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "file.sav";
+    private RecyclerView MeasurementList;
+    private Gson gson = new Gson();
+    private ArrayList input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * loads the data from the file object. I reused some of the code here from lab 2.
+     */
+    private void loadFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Type listType = new TypeToken<ArrayList<Measurement>>() {}.getType();
+            input = gson.fromJson(in, listType);
+        } catch (FileNotFoundException e) {
+            input = new ArrayList<Measurement>();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    /**
+     * Saves the set of measurements into the file.
+     */
+    private void saveFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, 0);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+
+            gson.toJson(input, writer);
+            writer.flush();
+            fos.close();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
